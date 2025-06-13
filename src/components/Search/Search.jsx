37dc4c5c7af3091374e3852a4ff14d29263c31cb
HalienCoder <nihaal.sivapuram@gmail.com>
2025-06-14 02:32:52 +0530

    feat: Add Search Box

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,16 @@
+import "./Search.css"
+import search from "../../assets/search.svg"
 export default function Search(){
     return(
-        <>
-            Search
-        </>
+        <div className="search-box">
+
+        <input className="condition" placeholder="Condition, procedure, speciality...">
+
+        </input>
+        <input className="location" placeholder="City, state, or zipcode"></input>
+        <input className="insurance" placeholder="Insurance Carrier"></input>
+        <button className="search"><img src={search} className="search-icon"/>Find Now</button>
+            
+        </div>
     )
 }
